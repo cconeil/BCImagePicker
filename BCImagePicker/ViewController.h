cfc8d64c9e7491c85df69934a4f3b90502a5375b
Chris O'Neil <cconeil5@gmail.com>
2014-12-08 02:43:11 -0500

    Initial Commit

---
+++ BCImagePicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BCImagePicker
+//
+//  Created by Chris O'Neil on 12/8/14.
+//  Copyright (c) 2014 Chris O'Neil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
